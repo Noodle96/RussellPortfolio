28a04cae60f78909a73e9e378a32911c1d93e1f3
Noodle96 <jorgealfredomarino2@gmail.com>
2025-08-03 14:11:56 -0500

    style: add hover effect for accent text and improve layout structure

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,9 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
+// Our components
+import Header from "@/components/Header";
+
 const jetbrainsMono = JetBrains_Mono({
     subsets: ["latin"],
     weight: ["100", "200", "300","400", "500", "600", "700","800"],
@@ -13,11 +16,12 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  return (
+    return (
     <html lang="en">
-      <body className={jetbrainsMono.variable}>
+        <body className={jetbrainsMono.variable}>
+        <Header />
         {children}
-      </body>
+        </body>
     </html>
   );
 }
