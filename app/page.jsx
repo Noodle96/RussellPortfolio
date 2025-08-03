041d74388491c16d7f397abd6b878b96521cbc77
Noodle96 <jorgealfredomarino2@gmail.com>
2025-08-03 14:13:11 -0500

    add the main page of contact, resume, services, work and main

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="font-primary text-accent bg-primary p-4 animate-accordion-down">
-        ¡Hola JetBrains Mono!
+        Home page
     </main>
   );
 }
