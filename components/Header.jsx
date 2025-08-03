8df67ac4bd0d5fb49a4b9214e12784849f0e8686
Noodle96 <jorgealfredomarino2@gmail.com>
2025-08-03 14:11:06 -0500

    Create the Header components

---
+++ components/Header.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Nav from "./Nav";
+
+const Header = () => {
+    return (
+        <header className="py-8 xl:py-12 text-white">
+            <div className="container mx-auto flex items-center justify-between">
+                {/* Logo */}
+                <Link href="/">
+                    <h1 className="text-4xl font-semibold">
+                        Russell<span className="text-accent">.</span>
+                    </h1>
+                </Link>
+                {/* desktop nav and  hire me button  */}
+                <div className="hidden xl:flex items-center gap-8"> {/* change to lg:flex*/}
+                    <Nav />
+                    <Link href="/contact">
+                        <Button className="ml-8">Hire Me</Button>
+                    </Link>
+                </div>
+
+                {/* mobile nav */}
+                <div className="xl:hidden">
+                    mobile nav
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
