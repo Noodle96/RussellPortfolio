6fd36d07e8050ca253f9f8e67c92c9efef6c9949
Noodle96 <jorgealfredomarino2@gmail.com>
2025-08-05 01:05:45 -0500

    feat: add page transition and stair animation components with framer-motion

---
+++ components/Stairs.jsx
@@ -0,0 +1,53 @@
+import {motion} from 'framer-motion';
+
+// variants
+const stairAnimation = {
+    initial:{
+        top: "0%",
+    },
+    animate:{
+        top: "100%",
+    },
+    exit:{
+        top:["100%", "0%"],
+    },
+};
+
+// calculate the reverse index for staggered effect
+const reverseIndex = (index) => {
+    const totalSteps = 7; // total number of steps
+    return totalSteps - index - 1; // reverse the index
+}
+
+const Stairs = () => {
+    return (
+        <>
+            {/* Render 6 motion divs, each representing a stair step,
+                each div will have the sae animation defined  by the stairAnimation object.
+                The delay for each div is calculated dinamically based on its reversed index.
+                creating a staggered effect with decreasing delay for each subsequent step.
+            */}
+            {[...Array(7)].map( (_, index) => {
+                return (
+                    <motion.div
+                        key={index}
+                        variants={stairAnimation}
+                        initial="initial"
+                        animate="animate"
+                        exit="exit"
+                        transition={{
+                            duration:0.4,
+                            ease: "easeInOut",
+                            delay: reverseIndex(index) * 0.1, // staggered delay based on
+                        }}
+                        className='h-full w-full bg-primary border-gradient-accent rounded-sm relative'
+                        // className="h-full w-full bg-primary stair-border-glow rounded-md"
+
+                    />
+                );
+            } )}
+        </>
+    )
+}
+
+export default Stairs
