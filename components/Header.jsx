c6d73bae648c40626a5cea5535b4a181fdc92ff8
Noodle96 <jorgealfredomarino2@gmail.com>
2025-08-08 18:40:24 -0500

    feat: implement MobileNav component for improved mobile navigation

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
     return (
@@ -22,7 +23,7 @@ const Header = () => {
 
                 {/* mobile nav */}
                 <div className="xl:hidden">
-                    mobile nav
+                    <MobileNav />
                 </div>
             </div>
         </header>
---
+++ components/MobileNav.jsx
@@ -0,0 +1,57 @@
+"use client";
+import { Sheet, SheetContent, SheetTrigger, SheetHeader, SheetTitle, SheetDescription } from "./ui/sheet";
+import Link from "next/link";
+import {CiMenuFries} from "react-icons/ci";
+import { usePathname } from "next/navigation";
+
+const links = [
+    {name: "home", path: "/"},
+    {name: "services", path: "/services"},
+    {name: "resume", path: "/resume"},
+    {name:"work", path: "/work"},
+    {name: "contact", path: "/contact"},
+];
+
+const MobileNav = () => {
+    const pathname = usePathname();
+    return (
+        <Sheet>
+            <SheetTrigger className="flex items-center justify-center">
+                <CiMenuFries className="text-[32px] text-accent" />
+            </SheetTrigger>
+
+            <SheetContent className="flex flex-col">
+                {/* Título accesible oculto */}
+                <SheetHeader>
+                    <SheetTitle className="sr-only">Mobile navigation</SheetTitle>
+                    <SheetDescription className="sr-only">
+                        Navigation drawer with links to main sections.
+                    </SheetDescription>
+                </SheetHeader>
+                {/* logo */}
+                <div className="mt-32 mb-40  text-center text-2xl">
+                    <Link href="/">
+                        <h1 className="text-4xl font-semibold">
+                            Russell<span className="text-accent">.</span>
+                        </h1>
+                    </Link>
+                </div>
+
+                {/* nav */}
+                <nav className="flex flex-col justify-center items-center gap-8">
+                    {links.map( (link, index) => {
+                        return(
+                            <Link href={link.path} key={index} className={`${link.path === pathname && "text-accent border-b-2"}
+                                capitalize font-medium hover:text-accent-hover transition-all `}>
+                                {link.name}
+                            </Link>
+                        );
+                    } )}
+                </nav>
+                
+            </SheetContent>
+        </Sheet>
+    )
+}
+
+export default MobileNav
