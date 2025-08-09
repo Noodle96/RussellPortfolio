f95ea13aaaebfbaf6e6555dddb8f961b38bd3f2b
Noodle96 <jorgealfredomarino2@gmail.com>
2025-08-09 02:02:53 -0500

    feat: enhance Home component with Photo, Social, and Stats components for improved layout and functionality

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,58 @@
-export default function Home() {
-  return (
-    <main className="font-primary text-accent bg-primary p-4 animate-accordion-down">
-        Home page
-    </main>
-  );
+import Photo from "@/components/Photo";
+import Social from "@/components/Social";
+import Stats from "@/components/Stats";
+import { Button } from "@/components/ui/button"
+import {FiDownload} from "react-icons/fi"
+
+const position1= "Software Engineer";
+const position2 = "Machine Learning";
+const position3 = "Data Science";
+
+const Home = () => {
+    return (
+        <section className="h-full">
+            <div className="container mx-auto h-full">
+                <div className="flex flex-col xl:flex-row  items-center justify-between xl:pt-8 xl:pb-24">
+                    {/* text */}
+                    <div className="text-center xl:text-left order-2 xl:order-none">
+                        <span className="text-xl">{position1}<span className="text-accent"> | </span>{position2}<span className="text-accent"> & </span>{position3}</span>
+                        <h1 className="h1  mt-6 mb-6 ">
+                            Hello I'm <br /> <span className="text-accent">Russell Tito</span>
+                        </h1>
+                        <p className="max-w-[650px] mb-9 text-white/80">
+                            I design and develop scalable software systems while leveraging machine
+                            learning and data science to create intelligent, efficient,
+                            and impactful solutions that solve real-world challenges across
+                            diverse domains.
+                        </p>
+
+                        {/* boton for social medias */}
+                        <div className="flex flex-col xl:flex-row items-center gap-8">
+                            <Button  variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                                <span>Download CV</span>
+                                <FiDownload className="ml-2 text-xl" />
+                            </Button>
+                            <div className="mb-8 xl:mb-0">
+                                <Social
+                                    containerStyles="flex  gap-6"
+                                    iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center
+                                                items-center text-accent text-base hover:bg-accent-hover
+                                                hover:transition-all duration-500"
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+
+                    {/* photo */}
+                    <div className="order-1 xl:order-none mb-8 xl:mb-0">
+                        <Photo />
+                    </div>
+                </div>
+            </div>
+            <Stats />
+        </section>
+    )
 }
+
+export default Home
