83bd2e82a04fcac23a2ad0b8ae1abb9f7e1ac383
Noodle96 <jorgealfredomarino2@gmail.com>
2025-11-26 15:10:38 -0500

    refactor: improve code formatting and update phone number in Contact and Resume pages

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,6 +1,6 @@
 "use client";
-import {FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs, FaPython} from "react-icons/fa";
-import {SiTailwindcss, SiNextdotjs} from "react-icons/si";
+import { FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs, FaPython } from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
 
 // about data
 const about = {
@@ -15,7 +15,7 @@ const about = {
         },
         {
             fieldName: "Phone",
-            fieldValue: "(+51) 931 149 426",
+            fieldValue: "(+51) 905 626 886",
         },
         {
             fieldName: "Experience",
@@ -179,17 +179,17 @@ const skills = {
     ],
 };
 
-import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
-import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
-import {ScrollArea} from "@/components/ui/scroll-area";
-import {motion} from "framer-motion";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
 const Resume = () => {
     return (
         <motion.div
-            initial={{opacity: 0}}
+            initial={{ opacity: 0 }}
             animate={{
                 opacity: 1,
-                transition: {delay: 2.4, duration: 0.4, ease: "easeIn"},
+                transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
             }}
             className=" min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
         >
@@ -211,8 +211,8 @@ const Resume = () => {
                                 <p className="max-w-[1400px] text-white/60 mx-auto xl:mx-0">{experience.description}</p>
                                 <ScrollArea className="h-[400px]">
                                     <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
-                                        {experience.items.map( (item, index) => {
-                                            return(
+                                        {experience.items.map((item, index) => {
+                                            return (
                                                 <li
                                                     key={index}
                                                     className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col \
@@ -226,7 +226,7 @@ const Resume = () => {
                                                     </div>
                                                 </li>
                                             );
-                                        } )}
+                                        })}
                                     </ul>
                                 </ScrollArea>
                             </div>
@@ -241,8 +241,8 @@ const Resume = () => {
                                 <p className="max-w-[1400px] text-white/60 mx-auto xl:mx-0">{education.description}</p>
                                 <ScrollArea className="h-[400px]">
                                     <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
-                                        {education.items.map( (item, index) => {
-                                            return(
+                                        {education.items.map((item, index) => {
+                                            return (
                                                 <li
                                                     key={index}
                                                     className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col \
@@ -256,7 +256,7 @@ const Resume = () => {
                                                     </div>
                                                 </li>
                                             );
-                                        } )}
+                                        })}
                                     </ul>
                                 </ScrollArea>
                             </div>
@@ -273,8 +273,8 @@ const Resume = () => {
                                     <p className="text-white/60 pb-3">{skills.description}</p>
                                 </div>
                                 <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px] gap-4">
-                                    {skills.skillList.map( (skill, index) => {
-                                        return(
+                                    {skills.skillList.map((skill, index) => {
+                                        return (
                                             <li key={index} >
                                                 <TooltipProvider delayDuration={100}>
                                                     <Tooltip>
@@ -290,7 +290,7 @@ const Resume = () => {
                                                 </TooltipProvider>
                                             </li>
                                         );
-                                    } )}
+                                    })}
                                 </ul>
                             </div>
                         </TabsContent>
@@ -302,14 +302,14 @@ const Resume = () => {
                                 <h3 className="text-4xl font-bold">{about.title}</h3>
                                 <p className="mx-auto xl:mx-0 text-white/60">{about.description}</p>
                                 <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[820px] mx-auto xl:mx-0">
-                                    {about.info.map( (item, index) => {
+                                    {about.info.map((item, index) => {
                                         return (
                                             <li key={index} className="flex items-center justify-center xl:justify-start gap-4">
                                                 <span className="text-white/60">{item.fieldName}<span className="text-accent">:</span></span>
                                                 <span className="text-xl">{item.fieldValue}</span>
                                             </li>
                                         );
-                                    } )}
+                                    })}
                                 </ul>
                             </div>
                         </TabsContent>
